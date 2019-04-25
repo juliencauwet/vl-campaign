1be2bec8548153e3e7d6c88af303c9793212c356
julien <juliencauwet@yahoo.fr>
2019-04-25 13:22:07 +0200

    first commit

---
+++ src/main/java/org/greenwin/VLCampaign/VlCampaignApplication.java
@@ -0,0 +1,13 @@
+package org.greenwin.VLCampaign;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class VlCampaignApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(VlCampaignApplication.class, args);
+	}
+
+}
---
+++ src/test/java/org/greenwin/VLCampaign/VlCampaignApplicationTests.java
@@ -0,0 +1,16 @@
+package org.greenwin.VLCampaign;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class VlCampaignApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
